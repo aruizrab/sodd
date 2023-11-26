35d7f7901819a493ff7bbd753ae4ce16b8cdcb0f
Alex Ruiz <68024691+aruizrab@users.noreply.github.com>
2023-11-26 18:11:45 +0100

    feature: added Event abstract class.

---
+++ Runtime/Events/Event.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace SODD.Events
+{
+    /// <summary>
+    ///     Represents a base event class for all scriptable object events.
+    /// </summary>
+    /// <typeparam name="T">The type of the event payload.</typeparam>
+    /// <remarks>
+    ///     This abstract class serves as the base for all specific event types. It implements the
+    ///     IEvent interface, allowing listeners to subscribe to or unsubscribe from events, and
+    ///     providing a method to invoke the event with a specific payload.
+    /// </remarks>
+    public abstract class Event<T> : ScriptableObject, IEvent<T>
+    {
+        /// <inheritdoc />
+        public void AddListener(Action<T> listener)
+        {
+            Listeners += listener;
+        }
+
+        /// <inheritdoc />
+        public void RemoveListener(Action<T> listener)
+        {
+            Listeners -= listener;
+        }
+
+        /// <inheritdoc />
+        public void Invoke(T payload)
+        {
+            Listeners?.Invoke(payload);
+        }
+
+        private event Action<T> Listeners;
+    }
+}
