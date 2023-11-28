eb333251351b91ed3deb3d5c5b6bbb8155929ab3
Alex Ruiz <68024691+aruizrab@users.noreply.github.com>
2023-11-28 10:48:37 +0100

    feature: added Vector3 events.

---
+++ Editor/Events/Vector3EventEditor.cs
@@ -0,0 +1,19 @@
+﻿using SODD.Events;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Events
+{
+    [CustomEditor(typeof(Vector3Event))]
+    public class Vector3EventEditor : UnityEditor.Editor
+    {
+        private Vector3 _payload;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            _payload = EditorGUILayout.Vector3Field("Payload", _payload);
+            if (GUILayout.Button("Invoke")) (target as Vector3Event)?.Invoke(_payload);
+        }
+    }
+}
---
+++ Runtime/Events/Vector3Event.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace SODD.Events
+{
+    /// <summary>
+    ///     A ScriptableObject representing an event that carries a Vector3 payload.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         The Vector3Event class extends the Event class for events that specifically handle three-dimensional
+    ///         vector data (Vector3). It is tailored for scenarios where 3D coordinates, directions, or other vector-based
+    ///         information in a three-dimensional space are integral to the event's purpose.
+    ///     </para>
+    ///     <para>
+    ///         Use this class to create and manage events involving 3D vector data within the Unity Editor. By registering
+    ///         listeners to a Vector3Event, developers can respond dynamically to changes or updates represented by Vector3
+    ///         values, such as position updates, movement directions, or spatial calculations.
+    ///     </para>
+    ///     <para>
+    ///         Vector3Event assets can be created and configured in the Unity Editor, enabling flexible and responsive event
+    ///         handling in various aspects of 3D game or application development.
+    ///     </para>
+    /// </remarks>
+    [CreateAssetMenu(menuName = Framework.Events.Vector3, fileName = nameof(Vector3Event),
+        order = Framework.MenuOrders.Vector3)]
+    public sealed class Vector3Event : Event<Vector3>
+    {
+    }
+}
